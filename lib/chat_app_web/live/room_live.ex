de8dfeedf041a4cd0a1153d38bae11831a31fe1c
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2025-02-25 02:14:59 +0900

    update room list

--- lib/chat_app_web/live/room_live.ex
+++ lib/chat_app_web/live/room_live.ex
@@ -2,19 +2,11 @@ defmodule ChatAppWeb.RoomLive do
   use ChatAppWeb, :live_view
 
   def mount(_params, _session, socket) do
-    room_name = "test"
+    room_list = ["初めまして", "おしゃべりルーム", "Elixir大好きルーム"]
     {
       :ok,
       socket
-        |> assign(:room_name, room_name)
-        |> assign(:counter, 0)
+        |> assign(:room_list, room_list)
     }
-  end
-
-  def handle_event("increase_counter", _params, socket) do
-    {:noreply, update(socket, :counter, &(&1 + 1))}
-  end
-  def handle_event("decrease_counter", _params, socket) do
-    {:noreply, update(socket, :counter, &(&1 - 1))}
   end
 end
